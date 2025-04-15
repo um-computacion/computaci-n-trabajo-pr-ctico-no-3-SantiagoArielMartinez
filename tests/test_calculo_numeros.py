9d35e115a4503a77367358f0de31c2f8a61fa29d
unknown <saar.martinez@alumno.um.edu.ar>
2025-04-15 15:48:04 -0300

    segunda issue, numeros negativos

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -14,7 +14,7 @@ class TestCalculoNumeros(unittest.TestCase):
     def test_ingreso_feliz(self, patch_input):
         numero = ingrese_numero()
         self.assertEqual(numero, 100)
-'''
+
     @patch(  # este patch controla lo que hace el input
         'builtins.input',
         return_value='-100'
@@ -22,7 +22,7 @@ class TestCalculoNumeros(unittest.TestCase):
     def test_ingreso_negativo(self, patch_input):
         with self.assertRaises(NumeroDebeSerPositivo):
             ingrese_numero()
-
+'''
     @patch(  # este patch controla lo que hace el input
         'builtins.input',
         return_value='AAA'
